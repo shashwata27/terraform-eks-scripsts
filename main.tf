d324fe6c28091dcb2d30ddf195e0d12b0ce2830d
Chandrakant Kaski <chandrakantkaski@Chandrakants-MacBook-Pro.local>
2023-02-15 14:48:24 +0530

    shashwat/chandrakant/aman | give fixed name for cluster

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
 locals {
-  cluster_name = "sha-cha-eks-${random_string.suffix.result}"
-}
-
-resource "random_string" "suffix" {
-  length  = 8
-  special = false
+  cluster_name = "sha-cha-eks-tf"
 }
